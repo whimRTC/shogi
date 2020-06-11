e1a39f17e93a974265b4f90af0b6fe4fc0be6ce3
Aitaro Chaya <aitaro.chaya@gmail.com>
2020-06-11 18:31:45 +0900

    step-2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
+import whimClientVue from "whim-client-vue";
+import "whim-client-vue/dist/whim-client-vue.css";
 
 Vue.config.productionTip = false;
+Vue.use(whimClientVue);
 
 new Vue({
   render: h => h(App)
