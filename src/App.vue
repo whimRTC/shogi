01ec862bf054664622cb0f8481751fbe17927617
Aitaro Chaya <aitaro.chaya@gmail.com>
2020-06-11 18:33:46 +0900

    step-3

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Main class="main" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Main: () => import("@/components/main/Index"),
   }
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/main/Index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="main">
+    <div class="row">
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+    </div>
+    <div class="row">
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+    </div>
+    <div class="row">
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+    </div>
+    <div class="row">
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+    </div>
+    <div class="row">
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+    </div>
+    <div class="row">
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+    </div>
+    <div class="row">
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+      <div class="col">王</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.main {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.row {
+  background: #b8883c70;
+  width: 90vw;
+  max-width: 60vh;
+  height: 18vw;
+  max-height: 12vh;
+  display: flex;
+}
+
+.col {
+  border: solid 1px;
+  width: 20%;
+  height: 100%;
+  box-sizing: border-box;
+  text-align: center;
+}
+</style>
