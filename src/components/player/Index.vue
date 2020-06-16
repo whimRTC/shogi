0e2ed1810bfa136b7d5965891429c0ef3776266f
Aitaro Chaya <aitaro.chaya@gmail.com>
2020-06-16 17:10:57 +0900

    色調整

--- src/components/player/Index.vue
+++ src/components/player/Index.vue
@@ -52,33 +52,77 @@ export default {
   font-weight: bold;
 }
 
-.border {
-  // border: solid;
-  box-sizing: border-box;
-  border-radius: 20px;
-  border-width: 10px;
-  // box-shadow: 0 0 0 10px blue inset, 0 0 0 20px red inset;
+// .border {
+//   // border: solid;
+//   // box-sizing: border-box;
+//   // border-radius: 20px;
+//   // border-width: 10px;
+//   // box-shadow: 0 0 0 10px blue inset, 0 0 0 20px red inset;
 
+//   @for $i from 0 to 7 {
+//     &.user-#{$i} {
+//       box-shadow: 0 0 0 10px rgba(map-get($user-colors, $i), 0.2) inset;
+//       &.active-#{$i} {
+//         box-shadow: 0 0 0 10px rgba(map-get($user-colors, $i), 0.8) inset;
+//       }
+//     }
+//   }
+//   @for $i from 0 to 7 {
+//     @for $j from 0 to 7 {
+//       &.user-#{$i}-#{$j} {
+//         box-shadow: 0 0 0 10px rgba(map-get($user-colors, $i), 0.2) inset,
+//           0px 0px 0 10px rgba(map-get($user-colors, $j), 0.2) inset;
+//         &.active-#{$i} {
+//           box-shadow: 0 0 0 10px rgba(map-get($user-colors, $i), 0.8) inset,
+//             0px 0px 0 10px rgba(map-get($user-colors, $j), 0.2) inset;
+//         }
+//         &.active-#{$j} {
+//           box-shadow: 0 0 0 10px rgba(map-get($user-colors, $i), 0.2) inset,
+//             0px 0px 0 10px rgba(map-get($user-colors, $j), 0.8) inset;
+//         }
+//       }
+//     }
+//   }
+// }
+.border {
+  box-sizing: border-box;
+  // border: 10px solid #0f0;
+  border-radius: 10px;
+  margin: 0 auto;
   @for $i from 0 to 7 {
     &.user-#{$i} {
-      box-shadow: 0 0 0 10px rgba(map-get($user-colors, $i), 0.2) inset;
+      // box-shadow: 0 0 0 10px rgba(map-get($user-colors, $i), 0.2) inset;
+      border: 10px solid rgba(map-get($user-colors, $i), 0.2);
       &.active-#{$i} {
-        box-shadow: 0 0 0 10px rgba(map-get($user-colors, $i), 0.8) inset;
+        border: 10px solid rgba(map-get($user-colors, $i), 0.9);
       }
     }
   }
+
   @for $i from 0 to 7 {
     @for $j from 0 to 7 {
       &.user-#{$i}-#{$j} {
-        box-shadow: 0 0 0 10px rgba(map-get($user-colors, $i), 0.2) inset,
-          0 0 0 20px rgba(map-get($user-colors, $j), 0.2) inset;
+        border: 10px solid rgba(map-get($user-colors, $i), 0.2);
         &.active-#{$i} {
-          box-shadow: 0 0 0 10px rgba(map-get($user-colors, $i), 0.8) inset,
-            0 0 0 20px rgba(map-get($user-colors, $j), 0.2) inset;
+          border: 10px solid rgba(map-get($user-colors, $i), 0.9);
         }
         &.active-#{$j} {
-          box-shadow: 0 0 0 10px rgba(map-get($user-colors, $i), 0.2) inset,
-            0 0 0 20px rgba(map-get($user-colors, $j), 0.8) inset;
+          &:before {
+            border: 10px solid rgba(map-get($user-colors, $j), 0.9);
+          }
+        }
+        &:before {
+          background: none;
+          border: 10px solid rgba(map-get($user-colors, $j), 0.2);
+          border-radius: 10px;
+          content: "";
+          display: block;
+          position: absolute;
+          top: 4px;
+          left: 4px;
+          right: 4px;
+          bottom: 4px;
+          pointer-events: none;
         }
       }
     }
